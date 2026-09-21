43795c13798f24c7d579fee19d38966a35490449
Reuben Roessler <68586512+rroessler@users.noreply.github.com>
2026-09-21 17:40:21 +1000

    fix: remove duplicate removal

--- tests/spread/boot-start-proxy/rollback.sh
+++ tests/spread/boot-start-proxy/rollback.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-systemctl stop squid
-apt-get purge --auto-remove squid-openssl
-rm -rf /etc/squid /var/lib/squid /var/spool/squid /var/log/squid /usr/lib/squid
-
 if [ -f /etc/iptables.rules ]; then
     iptables-restore < /etc/iptables.rules
     rm -f /etc/iptables.rules

64a43bd681455986d519f86d466da6d339d024af
Reuben Roessler <68586512+rroessler@users.noreply.github.com>
2026-09-21 17:32:37 +1000

    fix: also remove squid installattion

--- tests/spread/boot-start-proxy/rollback.sh
+++ tests/spread/boot-start-proxy/rollback.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+systemctl stop squid
+apt-get purge --auto-remove squid-openssl
+rm -rf /etc/squid /var/lib/squid /var/spool/squid /var/log/squid /usr/lib/squid
+
 if [ -f /etc/iptables.rules ]; then
     iptables-restore < /etc/iptables.rules
 fi
